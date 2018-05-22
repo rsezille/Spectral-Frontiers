cc9fedf933898ea370fd68bb0e0d3be949e5b204
Remi Sezille <r.sezille@gmail.com>
2018-05-22 14:23:19 +0200

    Add loader & game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/**
+ * GameManager is the same accross all scenes and is instantiated by the loader.
+ * Initialize missions, translations, monsters, etc.
+ */
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+
+    // Game initialization
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        Debug.Log("Game Manager awakes");
+    }
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/**
+ * Manage the creation of the GameManager and setup default configuration for each scene when debugging (to be able to access any screen immediately)
+ * Must be attached to every scene as the first GameObject
+ */
+public class Loader : MonoBehaviour {
+    public GameObject gameManager;
+
+    // Initialization
+    void Awake() {
+        if (GameManager.instance == null) {
+            Debug.Log("Game is starting...");
+            Instantiate(gameManager);
+        }
+    }
+}
