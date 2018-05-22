76199268593f9a6e397a2391b95c44572fb96b3e
Remi Sezille <r.sezille@gmail.com>
2018-05-22 19:38:28 +0200

    Add automatic boxcollider2D script

---
+++ Assets/Scripts/AutoBox2D.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+/**
+ * Add a collider2D on the attached GameObject.
+ * Useful on UI elements to get hit by raycasts
+ */
+[RequireComponent(typeof(RectTransform))]
+public class AutoBox2D : MonoBehaviour {
+    private BoxCollider2D box;
+    private RectTransform rect;
+
+    void Awake() {
+        if (rect == null) {
+            rect = gameObject.GetComponent<RectTransform>();
+        }
+
+        box = gameObject.AddComponent<BoxCollider2D>();
+
+        box.size = rect.sizeDelta;
+        box.offset = Vector2.zero;
+    }
+}
