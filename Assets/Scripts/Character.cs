9f839d3b00a3cc563c1581449bf852623a3da89e
Remi Sezille <r.sezille@gmail.com>
2018-05-22 15:54:00 +0200

    Add first draft of characters

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+/**
+ * Represent a character, which can be an allie or an enemy (a monster, etc.)
+ */
+public class Character {
+    public const int MAXIMUM_LEVEL = 50;
+
+    public enum Status {
+        None, Poisoned, Frozen, Slowed, Silenced, Sleeping
+    };
+
+    public string name;
+
+    public string sprite; //TODO: move this to job + type ?
+
+    // Global
+    public int level = 1;
+    public int experience = 0;
+    public Status status = Status.None;
+
+    // Health points
+    private int currentHP = 30; // Current health points (can be higher than the baseHP)
+    private int baseHP = 30; // Base health points without bonus (equipement...)
+    private int maximumHP; // Computed when changing equipement, taking a hit, and so on
+
+    // Magic points
+    private int currentMP = 0; // Current magic points (can be higher than the baseMP)
+    private int baseMP = 0; // Base magic points without bonus (equipement...)
+    private int maximumMP; // Computed when changing equipement, using a spell, and so on
+
+    public int basePhysicalAttack = 10;
+    public int basePhysicalDefense = 0;
+    public int baseMagicalPower = 10;
+    public int baseMagicalResistance = 0;
+
+    public int actionTokens = 1; // Number of times the character can do an action (basic attack/skill/spell/item) per turn
+    public int movementTokens = 1; // Number of times the character can move per turn
+
+    public int movementPoints = 3; // Number of squares the character can travel with one movement token
+
+    public Character(string name) {
+        this.name = name;
+        sprite = "ss_south"; //TODO
+
+        currentHP = ComputeMaxHP();
+    }
+
+    public int ComputeMaxHP() {
+        //TODO: Equipment impact
+        maximumHP = baseHP;
+
+        return maximumHP;
+    }
+
+    public int GetMaxHP() {
+        return maximumHP;
+    }
+
+    public int GetCurrentHP() {
+        return currentHP;
+    }
+
+    public void SetCurrentHP(int hp) {
+        currentHP = Mathf.Clamp(hp, 0, GetMaxHP());
+    }
+
+    public int ComputeMaxMP() {
+        //TODO: Equipement impact
+        maximumMP = baseMP;
+
+        return maximumMP;
+    }
+
+    public int GetMaxMP() {
+        return maximumMP;
+    }
+
+    public int GetCurrentMP() {
+        return currentMP;
+    }
+
+    public int GetPhysicalAttack() {
+        //TODO: Equipment impact
+        return basePhysicalAttack;
+    }
+
+    public int GetPhysicalDefense() {
+        //TODO: Equipment impact
+        return basePhysicalDefense;
+    }
+
+    public int GetMagicalPower() {
+        //TODO: Equipment impact
+        return baseMagicalPower;
+    }
+
+    public int GetMagicalResistance() {
+        //TODO: Equipment impact
+        return baseMagicalResistance;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,25 @@
-﻿public class Player {
+﻿using System.Collections.Generic;
+
+public class Player {
     public string playerName;
     public int gold;
+
+    public List<Character> ownedChars = new List<Character>();
 
     public Player() {
         playerName = Globals.DefaultPlayerName;
         gold = 0;
+
+        //TODO
+        Character ritz = new Character("Ritz");
+        Character mew = new Character("Mew");
+        Character kupo = new Character("Kupo");
+
+        ownedChars.Add(ritz);//TODO
+        ritz.SetCurrentHP(10);
+        ownedChars.Add(mew);//TODO
+        mew.SetCurrentHP(20);
+        ownedChars.Add(kupo);//TODO
+        kupo.SetCurrentHP(30);
     }
 }
