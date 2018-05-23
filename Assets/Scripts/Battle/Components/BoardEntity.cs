76fa578fc981005cadb12b3895e9122e5d5bd4f7
Remi Sezille <r.sezille@gmail.com>
2018-05-23 20:33:46 +0200

    First components

---
+++ Assets/Scripts/Battle/Components/BoardEntity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/**
+ * Represent an entity which is displayed on a square (or several).
+ * A BoardEntity is blocking (ie. only one BoardEntity per Square)
+ */
+ 
+public class BoardEntity : MonoBehaviour {
+    public enum Side {
+        Player, Enemy, Neutral
+    };
+    public Side side;
+
+    public Square[] squares; // The square(s) the entity is placed on
+}
---
+++ Assets/Scripts/Battle/Components/Movable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class Movable : MonoBehaviour {
+    public int movementPoints; // At the beginning of each turn, movementPoints = character.movementPoints
+    public int movementTokens; // At the beginning of each turn, movementTokens = character.movementTokens
+}
