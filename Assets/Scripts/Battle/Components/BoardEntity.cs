83b0288b048a3b1cb041dfb47ffe888a676e04dd
Remi Sezille <r.sezille@gmail.com>
2018-05-23 21:05:39 +0200

    Separate Side from BoardEntity

--- Assets/Scripts/Battle/Components/BoardEntity.cs
+++ Assets/Scripts/Battle/Components/BoardEntity.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 /**
  * Represent an entity which is displayed on a square (or several).
  * A BoardEntity is blocking (ie. only one BoardEntity per Square)
  */
- 
-public class BoardEntity : MonoBehaviour {
-    public enum Side {
-        Player, Enemy, Neutral
-    };
-    public Side side;
 
-    public Square[] squares; // The square(s) the entity is placed on
+public class BoardEntity : MonoBehaviour {
+    public int size = 1; // Number of squares the entity is taking (a fat boss not moving could take 10 squares)
+    public List<Square> squares; // The square(s) the entity is placed on
+
+    void Awake() {
+        squares = new List<Square>();
+    }
 }
