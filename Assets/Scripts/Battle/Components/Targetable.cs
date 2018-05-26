7b5227df0f9cfe45643c447a835af17fbc981da0
Remi Sezille <r.sezille@gmail.com>
2018-05-26 23:14:49 +0200

    Add targetable

---
+++ Assets/Scripts/Battle/Components/Targetable.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+/**
+ * Used for mainly against static board entities without health points (which are just BoardEntities, making the squares blocking)
+ */
+public class Targetable : MonoBehaviour {
+}
