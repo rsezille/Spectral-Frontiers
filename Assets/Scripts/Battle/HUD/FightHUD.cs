65541d0b33c2efd9110e16acf088b79f80b26852
Remi Sezille <r.sezille@gmail.com>
2018-05-30 19:20:34 +0200

    Add animations on fight HUD

--- Assets/Scripts/Battle/HUD/FightHUD.cs
+++ Assets/Scripts/Battle/HUD/FightHUD.cs
@@ -1,9 +1,16 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class FightHUD : MonoBehaviour {
     public GameObject moveButton;
+
+    public RectTransform fightMenu;
+    public RectTransform currentSquare;
+    public RectTransform selectedSquare;
+
+    private bool isGoingEnabled = false;
 
     private void Start() {
         moveButton.AddListener(EventTriggerType.PointerClick, BattleManager.instance.fight.Move);
@@ -14,5 +21,35 @@ public class FightHUD : MonoBehaviour {
         BoardChar boardChar = BattleManager.instance.currentBoardChar;
 
         moveButton.GetComponent<Button>().interactable = boardChar.movable.movementTokens > 0;
+    }
+
+    public void SetActiveWithAnimation(bool active) {
+        float speed = 0.6f;
+
+        if (active) {
+            isGoingEnabled = true;
+            this.gameObject.SetActive(true);
+
+            fightMenu.anchoredPosition3D = new Vector3(fightMenu.anchoredPosition3D.x, -120f, fightMenu.anchoredPosition3D.z);
+            fightMenu.DOAnchorPos3D(new Vector3(fightMenu.anchoredPosition3D.x, fightMenu.sizeDelta.y / 2f, fightMenu.anchoredPosition3D.z), speed).SetEase(Ease.OutCubic);
+
+            currentSquare.anchoredPosition3D = new Vector3(currentSquare.anchoredPosition3D.x, -120f, currentSquare.anchoredPosition3D.z);
+            currentSquare.DOAnchorPos3D(new Vector3(currentSquare.anchoredPosition3D.x, currentSquare.sizeDelta.y / 2f, currentSquare.anchoredPosition3D.z), speed).SetEase(Ease.OutCubic);
+
+            selectedSquare.anchoredPosition3D = new Vector3(selectedSquare.anchoredPosition3D.x, -120f, selectedSquare.anchoredPosition3D.z);
+            selectedSquare.DOAnchorPos3D(new Vector3(selectedSquare.anchoredPosition3D.x, selectedSquare.sizeDelta.y / 2f, selectedSquare.anchoredPosition3D.z), speed).SetEase(Ease.OutCubic);
+        } else {
+            isGoingEnabled = false;
+
+            fightMenu.DOAnchorPos3D(new Vector3(fightMenu.anchoredPosition3D.x, -120f, fightMenu.anchoredPosition3D.z), speed).SetEase(Ease.OutCubic);
+            currentSquare.DOAnchorPos3D(new Vector3(currentSquare.anchoredPosition3D.x, -120f, currentSquare.anchoredPosition3D.z), speed).SetEase(Ease.OutCubic);
+            selectedSquare.DOAnchorPos3D(new Vector3(selectedSquare.anchoredPosition3D.x, -120f, selectedSquare.anchoredPosition3D.z), speed).SetEase(Ease.OutCubic).OnComplete(DisableGameObject);
+        }
+    }
+
+    void DisableGameObject() {
+        if (isGoingEnabled) return;
+
+        gameObject.SetActive(false);
     }
 }
