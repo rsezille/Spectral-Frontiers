dac140a243da6c2ef7d945f7003d5bbf2efc14f7
Remi Sezille <r.sezille@gmail.com>
2018-06-03 14:45:30 +0200

    Update monsters with AI

--- Assets/Scripts/Battle/Monsters/Goblin.cs
+++ Assets/Scripts/Battle/Monsters/Goblin.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using SpriteGlow;
+using System.Collections;
 
 [RequireComponent(typeof(BoardEntity), typeof(Movable), typeof(Actionable))]
 [RequireComponent(typeof(Side), typeof(MouseReactive), typeof(SpriteGlowEffect))]
-[RequireComponent(typeof(BoardCharacter))]
-public class Goblin : MonoBehaviour {
-    // Components
-    public Side side;
-    public BoardCharacter boardCharacter;
+[RequireComponent(typeof(BoardCharacter), typeof(AI))]
+public class Goblin : MonoBehaviour, ICustomAI {
+    private CustomAI customAI;
 
     private void Awake() {
-        side = GetComponent<Side>();
-        boardCharacter = GetComponent<BoardCharacter>();
+        //customAI = gameObject.AddComponent<CustomAI>() as CustomAI;
+        //customAI.Use(this);
+    }
+
+    public IEnumerator ProcessAI() {
+        yield return null;
     }
 }
