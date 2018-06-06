b382346094171852492495871b2894b07d767f5a
Remi Sezille <r.sezille@gmail.com>
2018-06-06 16:07:06 +0200

    First draft of grid & placing tiles

---
+++ Assets/SFMapEditor/World.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class World : MonoBehaviour {
+    public Vector2Int size = new Vector2Int(3, 3);
+
+    public bool showGrid = true;
+    public bool drawMode = true;
+
+    public GameObject tile;
+
+    private void OnDrawGizmos() {
+        size.Clamp(new Vector2Int(1, 1), new Vector2Int(1000, 1000));
+
+        if (showGrid) {
+            Gizmos.color = Color.grey;
+
+            for (int x = 0; x <= size.x; x++) {
+                Gizmos.DrawLine(new Vector3(x, x / 2f), new Vector3(-size.y + x, (size.y + x) / 2f));
+            }
+
+            for (int y = 0; y <= size.y; y++) {
+                Gizmos.DrawLine(new Vector3(-y, y / 2f), new Vector3(size.x - y, (size.x + y) / 2f));
+            }
+        }
+    }
+}
---
+++ Assets/SFMapEditor/WorldEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(World))]
+public class WorldEditor : Editor {
+    World world;
+
+    private void OnEnable() {
+        world = (World)target;
+    }
+
+    private void OnSceneGUI() {
+        Debug.Log("Current event detected: " + Event.current.type);
+
+        Event e = Event.current;
+
+        if (world.drawMode) {
+            if (e.isMouse && e.type == EventType.MouseDown && e.button == 0) {
+                e.Use();
+
+                Vector3 tilePosition = Camera.current.ScreenToWorldPoint(new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight));
+
+                Instantiate(world.tile, new Vector3(tilePosition.x, tilePosition.y, Camera.main.transform.position.z), Quaternion.identity);
+
+                
+            }
+
+            Selection.activeGameObject = world.gameObject;
+        }
+    }
+}
