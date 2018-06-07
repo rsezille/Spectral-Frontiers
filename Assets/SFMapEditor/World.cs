b33132da98b5d9cb34b3b64e1c2627a3fddc5885
Remi Sezille <r.sezille@gmail.com>
2018-06-07 23:58:16 +0200

    Second update of the editor

--- Assets/SFMapEditor/World.cs
+++ Assets/SFMapEditor/World.cs
@@ -8,8 +8,18 @@ public class World : MonoBehaviour {
     public Color gridColor = Color.grey;
 
     public GameObject tile;
+    public GameObject tileTwo;
+
+    public GameObject map;
+
+    public bool useTileOne = true;
+
+    private void Awake() {
+    }
 
     private void OnDrawGizmos() {
+        if (!map) map = new GameObject("Map");
+
         size.Clamp(new Vector2Int(1, 1), new Vector2Int(1000, 1000));
 
         if (showGrid) {
@@ -22,6 +32,16 @@ public class World : MonoBehaviour {
             for (int y = 0; y <= size.y; y++) {
                 Gizmos.DrawLine(new Vector3(-y, y / 2f), new Vector3(size.x - y, (size.x + y) / 2f));
             }
+
+            /*Gizmos.color = Color.white;
+
+            for (int x = 0; x <= size.x; x++) {
+                Gizmos.DrawLine(new Vector3(x, x / 2f + 0.5f), new Vector3(-size.y + x, (size.y + x) / 2f + 0.5f));
+            }
+
+            for (int y = 0; y <= size.y; y++) {
+                Gizmos.DrawLine(new Vector3(-y, y / 2f + 0.5f), new Vector3(size.x - y, (size.x + y) / 2f + 0.5f));
+            }*/
         }
     }
 }
--- Assets/SFMapEditor/WorldEditor.cs
+++ Assets/SFMapEditor/WorldEditor.cs
@@ -1,16 +1,32 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [CustomEditor(typeof(World))]
 public class WorldEditor : Editor {
     World world;
 
+    private bool clickToUp = false;
+
     private void OnEnable() {
         world = (World)target;
     }
 
+    private void EditorToolbox(int windowID) {
+        world.showGrid = GUI.Toggle(new Rect(10, 20, 100, 20), world.showGrid, "Toggle grid");
+        clickToUp = GUI.Toggle(new Rect(10, 40, 100, 20), clickToUp, "ClickToUp");
+
+        GUI.DragWindow(new Rect(0, 0, 10000, 20));
+    }
+    
     private void OnSceneGUI() {
         Event e = Event.current;
+
+        Handles.BeginGUI();
+        GUI.Window(0, new Rect(20, 20, 120, 80), EditorToolbox, "SFMapBuilder");
+        
+
+        Handles.EndGUI();
 
         if (world.drawMode) {
             if (e.isMouse && e.type == EventType.MouseDown && e.button == 0) {
@@ -19,20 +35,65 @@ public class WorldEditor : Editor {
                 Vector3 mousePosition = new Vector3(e.mousePosition.x, -e.mousePosition.y + Camera.current.pixelHeight);
                 Vector3 localMousePos = Camera.current.ScreenToWorldPoint(mousePosition);
 
-                Debug.Log(localMousePos);
-
                 // Square coords
                 int Sx = Mathf.FloorToInt((localMousePos.x / 2) + localMousePos.y);
                 int Sy = Mathf.FloorToInt(localMousePos.y - (localMousePos.x / 2));
+
+                if (Sx < 0 || Sx >= world.size.x || Sy < 0 || Sy >= world.size.y) return;
+
+                GameObject square = GameObject.Find("Square(" + Sx + "," + Sy + ")");
+
+                if (clickToUp) {
+                    if (square) {
+                        SpriteRenderer highestTile = null;
+
+                        SpriteRenderer[] sprites = square.GetComponentsInChildren<SpriteRenderer>();
+
+                        foreach (SpriteRenderer sprite in sprites) {
+                            if (highestTile == null || sprite.sortingOrder > highestTile.sortingOrder) {
+                                highestTile = sprite;
+                            }
+                        }
+
+                        if (highestTile != null) {
+                            highestTile.transform.Translate(new Vector3(0f, 5f / Globals.TileHeight));
+                        }
+                    }
+
+                    return;
+                }
 
                 // Center of the square
                 float Cx = Sx - Sy;
                 float Cy = (Sx + Sy + 1f) / 2f;
 
-                Debug.Log("Square: " + Sx + "," + Sy + " / Center: " + Cx + "," + Cy);
+                GameObject tile = world.useTileOne ? world.tile : world.tileTwo;
 
-                GameObject go = Instantiate(world.tile, new Vector3(Cx, Cy, Camera.main.transform.position.z), Quaternion.identity);
-                go.GetComponent<SpriteRenderer>().sortingOrder = -(world.size.y * Sy + Sx);
+                int highestSortingOrder = 0;
+
+                if (!square) {
+                    square = new GameObject("Square(" + Sx + "," + Sy + ")");
+                    SortingGroup sortingGroup = square.AddComponent<SortingGroup>();
+                    sortingGroup.sortingOrder = -(world.size.x * Sy + Sx);
+                    square.transform.SetParent(world.map.transform);
+                } else {
+                    SpriteRenderer[] sprites = square.GetComponentsInChildren<SpriteRenderer>();
+
+                    
+
+                    foreach (SpriteRenderer sprite in sprites) {
+                        if (sprite.sortingOrder > highestSortingOrder) {
+                            highestSortingOrder = sprite.sortingOrder;
+                        }
+                    }
+
+                    highestSortingOrder++;
+                }
+
+                GameObject go = Instantiate(tile, new Vector3(Cx, Cy, 0f), Quaternion.identity);
+                go.name = "Tile";
+                go.transform.SetParent(square.transform);
+                go.GetComponent<SpriteRenderer>().sortingOrder = highestSortingOrder;
 
             }
 
