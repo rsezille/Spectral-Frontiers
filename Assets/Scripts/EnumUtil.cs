3a0039b31e829133d90629b5ffa85c5811581dcd
Remi Sezille <r.sezille@gmail.com>
2018-07-05 17:59:52 +0200

    Add utility method to simplify enum parsing

---
+++ Assets/Scripts/EnumUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class EnumUtil {
+    public static T ParseEnum<T>(string value, T defaultValue) where T : struct {
+        if (string.IsNullOrEmpty(value)) {
+            return defaultValue;
+        }
+
+        T result;
+
+        return Enum.TryParse<T>(value, true, out result) ? result : defaultValue;
+    }
+}
