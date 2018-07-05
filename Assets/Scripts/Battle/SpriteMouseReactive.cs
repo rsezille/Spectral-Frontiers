52f55a57ec1853d6f4799b3c1db9366eef730511
Remi Sezille <r.sezille@gmail.com>
2018-07-05 21:13:42 +0200

    Add a sub mouse reactive for board characters

---
+++ Assets/Scripts/Battle/SpriteMouseReactive.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(MouseReactive))]
+public class SpriteMouseReactive : MonoBehaviour {
+    // Components
+    private MouseReactive mouseReactive;
+
+    private BoardCharacter boardCharacter;
+
+    private void Awake() {
+        boardCharacter = GetComponentInParent<BoardCharacter>();
+
+        mouseReactive = GetComponent<MouseReactive>();
+
+        mouseReactive.MouseEnter = new UnityEvent();
+        mouseReactive.MouseEnter.AddListener(MouseEnter);
+        mouseReactive.MouseLeave = new UnityEvent();
+        mouseReactive.MouseLeave.AddListener(MouseLeave);
+        mouseReactive.Click = new UnityEvent();
+        mouseReactive.Click.AddListener(Click);
+    }
+
+    /**
+     * Triggered by Board
+     */
+    public void MouseEnter() {
+        if (boardCharacter != null) {
+            boardCharacter.MouseEnter();
+        }
+    }
+
+    /**
+     * Triggered by Board
+     */
+    public void MouseLeave() {
+        if (boardCharacter != null) {
+            boardCharacter.MouseLeave();
+        }
+    }
+
+    /**
+     * Triggered by Board
+     */
+    public void Click() {
+        if (boardCharacter != null) {
+            boardCharacter.Click();
+        }
+    }
+}
