4fa4f899c76c16db88df6a9385fab6014e287611
Remi Sezille <r.sezille@gmail.com>
2018-07-07 13:59:21 +0200

    Add semi transparent entities

---
+++ Assets/SFMapEditor/Scripts/Components/SFSemiTransparent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
+public class SFSemiTransparent : MonoBehaviour {
+    private SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void MouseEnter() {
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 0.5f);
+    }
+
+    public void MouseLeave() {
+        spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, 1f);
+    }
+}
