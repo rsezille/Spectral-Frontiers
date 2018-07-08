d12aa751235fa5c51526f9ea0a8f59c70d794cc6
Remi Sezille <r.sezille@gmail.com>
2018-07-08 15:22:50 +0200

    Add keybind

---
+++ Assets/Scripts/Core/KeyBind.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace SF {
+    /**
+     * TODO: Delete this and use a professionnal input manager instead
+     */
+    public class KeyBind {
+        public KeyCode defaultKey { get; private set; }
+        public KeyCode bindedKey { get; private set; }
+        private bool bindable = false;
+
+        public KeyBind(KeyCode defaultKey, bool bindable = false) {
+            this.defaultKey = defaultKey;
+            bindedKey = defaultKey;
+            this.bindable = bindable;
+        }
+
+        public bool IsKeyDown {
+            get {
+                return Input.GetKeyDown(bindedKey);
+            }
+        }
+
+        public bool IsKeyHeld {
+            get {
+                return Input.GetKey(bindedKey);
+            }
+        }
+
+        public void Bind(KeyCode key) {
+            if (bindable) {
+                bindedKey = key;
+            }
+        }
+
+        public void ResetKey() {
+            bindedKey = defaultKey;
+        }
+    }
+}
