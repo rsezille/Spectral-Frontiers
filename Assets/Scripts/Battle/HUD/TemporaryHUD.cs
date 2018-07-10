77ceb781dccc6a82c1709981e6a7f5f7655e94e1
Remi Sezille <r.sezille@gmail.com>
2018-07-10 14:02:09 +0200

    Add a temporary HUD to view some information

---
+++ Assets/Scripts/Battle/HUD/TemporaryHUD.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemporaryHUD : MonoBehaviour {
+    private BattleManager battleManager;
+
+    public Text text;
+
+    private void Awake() {
+        battleManager = BattleManager.instance;
+    }
+
+    private void Update() {
+        text.text = "Version: " + Application.version + "\n";
+        text.text += "-------------------\n";
+        text.text += "Battle step: " + battleManager.currentBattleStep + "\n";
+
+        if (battleManager.currentBattleStep == BattleManager.BattleStep.Fight) {
+            text.text += "Fight step: " + battleManager.currentTurnStep + "\n";
+            text.text += "Turn: " + battleManager.turn + "\n";
+        } else if (battleManager.currentBattleStep == BattleManager.BattleStep.Placing) {
+            text.text += "Placed characters: " + battleManager.playerCharacters.Count + "/" + battleManager.mission.max_player_characters + "\n";
+        }
+    }
+}
