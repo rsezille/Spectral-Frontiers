8345101228d2151adf3ff97105abe653237d4ab6
Remi Sezille <r.sezille@gmail.com>
2018-07-14 14:42:40 +0200

    First draft of dialogbox

--- Assets/Scripts/DialogBox.cs
+++ Assets/Scripts/DialogBox.cs
@@ -10,17 +10,42 @@ public class DialogBox : MonoBehaviour {
         Instant = 0,
     };
 
+    public Canvas[] presets; // 0 is the default one
+
+    private void Awake() {
+        if (presets == null) {
+            presets = new Canvas[0]; // Avoid undefined
+        }
+
+        if (presets.Length > 0) {
+            foreach (Canvas preset in presets) {
+                preset.gameObject.SetActive(false);
+            }
+        }
+    }
+
     /**
      * Show a global dialog box
      * Can set a name if wanna display one
      */
-    public void Show(int preset, string name = "") {
-        Debug.Log("Show:   " + preset);
+    public void Show(int presetIndex, string name = "") {
+        Debug.Log("Show:   " + presetIndex);
+
+        if (presets.Length == 0) {
+            Debug.LogWarning("No preset set, dialogbox will not be shown");
+        }
+
+        if (presetIndex >= presets.Length) {
+            Debug.LogWarning("Preset out of bounds, will use default preset");
+            presetIndex = 0;
+        }
+
+        presets[presetIndex].gameObject.SetActive(true);
 
         //ResetAllProperties();
         //EnablePreset(preset);   // The dialogbox prefab comes with several sub game objects containing a background and a canvas with a textmesh pro text
-                                // Allowing to customize the text per background and so on
-                                // Should also contain a little arrow to point the dialogbox to the character
+        // Allowing to customize the text per background and so on
+        // Should also contain a little arrow to point the dialogbox to the character
         //GetTextFromPreset(); // Just do a preset.GetComponentInChildren<TextMesh>
         //EnableDialogBox(); // By enabling the game object and try to use DOTween to animate the text ?
     }
