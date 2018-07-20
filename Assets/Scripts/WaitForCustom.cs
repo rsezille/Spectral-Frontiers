a2c9b37a925120d86a6bd40f3d6b71ad3abebd8c
Remi Sezille <r.sezille@gmail.com>
2018-07-20 16:27:49 +0200

    Add custom yield instruction

---
+++ Assets/Scripts/WaitForCustom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/**
+ * Used to transform any Monobehavior into a coroutine, by implementing the IWaitForCustom interface and returning itself in a method.
+ * See DialogBox.Show for an example
+ */
+public class WaitForCustom : CustomYieldInstruction {
+    private IWaitForCustom waitForCustom;
+
+    public override bool keepWaiting {
+        get {
+            return !waitForCustom.IsFinished();
+        }
+    }
+
+    public WaitForCustom(IWaitForCustom waitForCustom) {
+        this.waitForCustom = waitForCustom;
+    }
+}
