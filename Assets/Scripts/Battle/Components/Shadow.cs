0eecd080cb41857b753eb24d3c34affb7de1a72e
Remi Sezille <r.sezille@gmail.com>
2018-07-23 20:17:21 +0200

    Feat(Battle): Add shadows under entities

---
+++ Assets/Scripts/Battle/Components/Shadow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Shadow : MonoBehaviour {
+    public SpriteRenderer shadowPrefab;
+
+    private Transform instance;
+
+    private void Awake() {
+        instance = Instantiate(shadowPrefab, transform).transform;
+    }
+
+    private void Start() {
+        // Don't show a scaled shadow if the parent is self scaled
+        instance.localScale = new Vector3(1f / instance.lossyScale.x, 1f / instance.lossyScale.y, 1f / instance.lossyScale.z);
+    }
+}
