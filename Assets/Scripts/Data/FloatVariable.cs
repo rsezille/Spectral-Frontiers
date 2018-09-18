ea35ba21086556302641af6642c89e998f2c9fff
Remi Sezille <r.sezille@gmail.com>
2018-09-18 15:47:24 +0200

    Feat: Add SO gold

---
+++ Assets/Scripts/Data/FloatVariable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SF/FloatVariable")]
+public class FloatVariable : ScriptableObject {
+#if UNITY_EDITOR
+    [Multiline]
+    public string developerDescription = "";
+#endif
+    public float value;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,14 @@
 
 public class Player {
     public string playerName;
-    public int gold;
 
     public List<Character> characters = new List<Character>();
 
     public Player() {
         playerName = Globals.DefaultPlayerName;
-        gold = 0;
 
         // TODO [ALPHA] Use scenario to load default characters
-        Character ritz = new Character("Hero");
+        Character ritz = new Character(playerName);
         Character mew = new Character("Mew");
         Character kupo = new Character("Mewtwo");
 
