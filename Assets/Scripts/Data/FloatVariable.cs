7b5725e7a78c4da8d16058a17f28ec1fea7027c0
Remi Sezille <r.sezille@gmail.com>
2018-09-18 17:56:17 +0200

    Style: Variable name

--- Assets/Scripts/Data/FloatVariable.cs
+++ Assets/Scripts/Data/FloatVariable.cs
@@ -4,7 +4,7 @@
 public class FloatVariable : ScriptableObject {
 #if UNITY_EDITOR
     [Multiline]
-    public string developerDescription = "";
+    public string internalNotes = "";
 #endif
     public float value;
 }
