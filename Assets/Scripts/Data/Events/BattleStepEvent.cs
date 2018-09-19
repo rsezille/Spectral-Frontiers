57e163e24aa7396b692b4c052b1d15ad5ec269d6
Remi Sezille <r.sezille@gmail.com>
2018-09-19 14:03:24 +0200

    Feat: Add events with unityevent and SO

---
+++ Assets/Scripts/Data/Events/BattleStepEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF {
+    [CreateAssetMenu(menuName = "SF/Events/BattleStepEvent")]
+    public class BattleStepEvent : ScriptableObject {
+        private readonly List<BattleStepEventListener> listeners = new List<BattleStepEventListener>();
+
+        public void Raise(BattleState.BattleStep battleStep) {
+            for (int i = listeners.Count - 1; i >= 0; i--) {
+                listeners[i].OnEventRaised(battleStep);
+            }
+        }
+
+        public void RegisterListener(BattleStepEventListener listener) {
+            if (!listeners.Contains(listener)) {
+                listeners.Add(listener);
+            }
+        }
+
+        public void UnregisterListener(BattleStepEventListener listener) {
+            if (listeners.Contains(listener)) {
+                listeners.Remove(listener);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Data/Events/BattleStepEventEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SF {
+    [CustomEditor(typeof(BattleStepEvent))]
+    public class BattleStepEventEditor : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            BattleStepEvent e = target as BattleStepEvent;
+
+            if (GUILayout.Button("Raise")) {
+                e.Raise(BattleState.BattleStep.Cutscene);
+            }
+        }
+    }
+}
