8aa467db820c0dfa3d9118d17975b3ff03a13536
Remi Sezille <r.sezille@gmail.com>
2018-09-20 23:09:32 +0200

    Refactor: Add glow settings for characters

---
+++ Assets/Scripts/Data/GlowSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF {
+    [CreateAssetMenu(menuName = "SF/Settings/Glow")]
+    public class GlowSettings : ScriptableObject {
+        public float minBrightness = 1f;
+        public float maxBrightness = 4.5f;
+        [Tooltip("Time to do a 1/2 cycle")]
+        public float time = 1f;
+    }
+}
