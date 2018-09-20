e153807b00b54399a3f718cc630c271e514b82d3
Remi Sezille <r.sezille@gmail.com>
2018-09-20 08:41:07 +0200

    Fix: Add missing mission so

---
+++ Assets/Scripts/Data/MissionVariable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SF {
+    [CreateAssetMenu(menuName = "SF/MissionVariable")]
+    public class MissionVariable : ScriptableObject {
+#if UNITY_EDITOR
+        [Multiline]
+        public string internalNotes = "";
+#endif
+
+        public RawMission value;
+    }
+}
