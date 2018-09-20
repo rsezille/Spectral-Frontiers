2eeb6c1b28e184f5b1b4945cc7d9ad1b283c6432
Remi Sezille <r.sezille@gmail.com>
2018-09-20 18:47:05 +0200

    Feat: Add stat SO and drafts

---
+++ Assets/Scripts/Data/Stat.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "SF/Stat")]
+public class Stat : ScriptableObject {
+    public string name;
+
+    public int maxValue;
+    public int minValue;
+}
