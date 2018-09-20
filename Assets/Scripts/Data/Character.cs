aed02d7dbca127f3b2999641e58277f88eec9c44
Remi Sezille <r.sezille@gmail.com>
2018-09-20 18:59:58 +0200

    Feat: Add character SO and drafts

---
+++ Assets/Scripts/Data/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace SF {
+    [CreateAssetMenu(menuName = "SF/Character")]
+    public class Character : ScriptableObject {
+        [Serializable]
+        public struct CharacterStat {
+            public Stat statType;
+            public int currentValue;
+        }
+
+        public StringReference playerName;
+
+        public CharacterStat hp;
+        public int currentHp;
+        public CharacterStat atk;
+        public CharacterStat spd;
+    }
+}
