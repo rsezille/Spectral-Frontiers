9ce2060dfd40f2c21a84c13c531adc6b19ae1828
Remi Sezille <r.sezille@gmail.com>
2018-09-20 22:58:03 +0200

    Refactor: Split shadow into component and controller

---
+++ Assets/Scripts/Scenes/Battle/Components/ShadowComponent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace SF {
+    public class ShadowComponent : MonoBehaviour {
+        public ShadowController shadowPrefab;
+
+        private void Awake() {
+            Instantiate(shadowPrefab, transform);
+        }
+    }
+}
