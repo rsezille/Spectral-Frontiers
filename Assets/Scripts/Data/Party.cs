82611a922819f73a937a424df51fd32647cd2207
Remi Sezille <r.sezille@gmail.com>
2018-09-21 16:01:17 +0200

    Feat: Add party SO

---
+++ Assets/Scripts/Data/Party.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF {
+    [CreateAssetMenu(menuName = "SF/Systems/Party")]
+    public class Party : ScriptableObject {
+        public List<Character> characters;
+    }
+}
