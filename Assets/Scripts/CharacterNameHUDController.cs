7d132adf5ed1fa704a7529949554731874ae9d3d
Remi Sezille <r.sezille@gmail.com>
2018-09-21 16:00:54 +0200

    Refactor: Add controllers for health bar and char name

---
+++ Assets/Scripts/CharacterNameHUDController.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace SF {
+    public class CharacterNameHUDController : MonoBehaviour {
+        private TextMeshProUGUI characterName;
+
+        [Header("Dependencies")]
+        public Character character;
+
+        private void Awake() {
+            if (!characterName) {
+                characterName = GetComponentInChildren<TextMeshProUGUI>();
+            }
+        }
+
+        private void Start() {
+            characterName.SetText(character.characterName);
+        }
+    }
+}
---
+++ Assets/Scripts/HealthBarHUDController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace SF {
+    public class HealthBarHUDController : MonoBehaviour {
+        public ChangeColorByScale hpScale;
+
+        [Header("Dependencies")]
+        public Character character;
+
+        private void Awake() {
+            if (!hpScale) {
+                hpScale = GetComponentInChildren<ChangeColorByScale>();
+            }
+        }
+
+        private void Start() {
+            UpdateHPScale();
+        }
+
+        private void Update() {
+            UpdateHPScale();
+        }
+
+        private void UpdateHPScale() {
+            hpScale.transform.localScale = new Vector3(
+                (float)character.currentHp / (float)character.hp.currentValue,
+                hpScale.transform.localScale.y,
+                hpScale.transform.localScale.z
+            );
+        }
+    }
+}
