116cfa0c50dd4ac2681c35e9e762c700bf4f8126
Remi Sezille <r.sezille@gmail.com>
2018-09-23 17:32:45 +0200

    Feat: Add items

---
+++ Assets/Scripts/Data/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF {
+    [CreateAssetMenu(menuName = "SF/Item")]
+    public class Item : ScriptableObject {
+        public string name;
+
+        [Serializable]
+        public struct StatModifier {
+            public Stat stat;
+            public int increase;
+        }
+
+        public List<StatModifier> statModifier;
+    }
+}
