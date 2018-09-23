d2bd8ab0a09da06b66a1831e2a1045b6bc7b1e37
Remi Sezille <r.sezille@gmail.com>
2018-09-23 17:34:41 +0200

    Feat: Add inventory

---
+++ Assets/Scripts/Data/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SF {
+    [CreateAssetMenu(menuName = "SF/Systems/Inventory")]
+    public class Inventory : ScriptableObject {
+        [Serializable]
+        public struct ItemStack {
+            public Item item;
+            public int quantity;
+        }
+
+        public List<ItemStack> itemStacks;
+    }
+}
