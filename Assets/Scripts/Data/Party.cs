919ba3fb9af65c8a5b3542ad55c408cad119f51f
Remi Sezille <r.sezille@gmail.com>
2018-09-24 16:59:24 +0200

    Refactor: Add utility methods

--- Assets/Scripts/Data/Party.cs
+++ Assets/Scripts/Data/Party.cs
@@ -5,5 +5,33 @@ namespace SF {
     [CreateAssetMenu(menuName = "SF/Systems/Party")]
     public class Party : ScriptableObject {
         public List<Character> characters;
+
+        public Character GetNextCharacter(Character currentCharacter) {
+            int index = characters.IndexOf(currentCharacter);
+
+            if (index == -1) {
+                return null;
+            }
+
+            if (index >= characters.Count - 1) {
+                return characters[0];
+            } else {
+                return characters[index + 1];
+            }
+        }
+
+        public Character GetPreviousCharacter(Character currentCharacter) {
+            int index = characters.IndexOf(currentCharacter);
+
+            if (index == -1) {
+                return null;
+            }
+
+            if (index <= 0) {
+                return characters[characters.Count - 1];
+            } else {
+                return characters[index - 1];
+            }
+        }
     }
 }
