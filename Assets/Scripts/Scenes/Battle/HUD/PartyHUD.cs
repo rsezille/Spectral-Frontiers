128f2e675570a942dfda396dabee5f4d03c466d2
Remi Sezille <r.sezille@gmail.com>
2018-10-21 14:28:44 +0200

    Feat: First draft of Party HUD

---
+++ Assets/Scripts/Scenes/Battle/HUD/PartyHUD.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace SF {
+    public class PartyHUD : MonoBehaviour {
+        [Header("Dependencies")]
+        public Party party;
+
+        public void Show() {
+
+        }
+
+        public void Show(Character character) {
+
+        }
+    }
+}
